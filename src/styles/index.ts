9c7257394fa004d6a88df8d13dbb6517bf0d9104
Jean Lincoln <jeanlincol@gmail.com>
2022-11-14 16:13:50 -0300

    Estrutura visual - Estrutura da Home

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,4 +1,4 @@
-import { createStitches } from "@stitches/react";
+import { createStitches } from '@stitches/react';
 
 export const { config, styled, css, globalCss, keyframes, getCssText, theme, createTheme } =
   createStitches({
@@ -13,6 +13,12 @@ export const { config, styled, css, globalCss, keyframes, getCssText, theme, cre
 
         green500: `#00875f`,
         green300: `#00b37e`,
+      },
+      fontSizes: {
+        md: '1.125rem',
+        lg: '1.25rem',
+        xl: '1.5rem',
+        '2xl': '2rem',
       },
     },
   });
