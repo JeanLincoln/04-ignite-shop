cd07feb5281c25aa8187939acfaf097ec32f229d
Jean Lincoln <jeanlincol@gmail.com>
2022-11-20 13:22:30 -0300

    Produto & Checkout - Botão de comprar

--- src/styles/pages/product.ts
+++ src/styles/pages/product.ts
@@ -63,7 +63,14 @@ export const ProductDetails = styled('div', {
     fontSize: '$md',
 
     '&:hover': {
-      backgroundColor: '$green300',
+      '&:disabled': {
+        opacity: 0.6,
+        cursor: 'not-allowed',
+      },
+
+      '&:not(:disabled):hover': {
+        backgroundColor: '$green300',
+      },
     },
   },
 });
